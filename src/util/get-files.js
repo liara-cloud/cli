e646b829393bad9cde3516e3a19d6718f78a4ff3
Mhe Mrg <mhemrg120@gmail.com>
2017-11-20 13:16:44 +0330

    fix bug in walking through large projects

--- src/util/get-files.js
+++ src/util/get-files.js
@@ -1,12 +1,11 @@
 import klaw from 'klaw';
 import hash from './hash';
-import { queue } from 'async';
 import through2 from 'through2';
 import { relative } from 'path';
 import { readFile } from 'fs-extra';
 
-const excludeDir = through2.obj(function (item, enc, next) {
-  if ( ! item.stats.isDirectory()) this.push(item);
+const filterFiles = through2.obj(function (item, enc, next) {
+  if (item.stats.isFile()) this.push(item);
   next();
 });
 
@@ -14,40 +13,40 @@ export default async function getFiles(projectPath) {
   const mapHashesToFiles = new Map;
   
   await new Promise(resolve => {
-    const hashesQueue = queue(async ({ path, stats }, callback) => {
-      const data = await readFile(path);
-      const checksum = hash(data);
-
-      const file = {
-        checksum,
-        path: relative(projectPath, path),
-        mode: stats.mode,
-        size: stats.size,
-      };
-
-      if(mapHashesToFiles.has(checksum)) {
-        const { files } = mapHashesToFiles.get(checksum);
-        mapHashesToFiles.set(checksum, {
-          data,
-          files: [...files, file],
-        });
-      } else {
-        mapHashesToFiles.set(checksum, {
-          data,
-          files: [file],
-        });
-      }
-
-      callback();
-    }, 15);
-
-    hashesQueue.drain = () => resolve(mapHashesToFiles);
+    const files = [];
 
     klaw(projectPath)
-      .pipe(excludeDir)
-      .on('data', hashesQueue.push);
-  });
+      .pipe(filterFiles)
+      .on('data', file => files.push(file))
+      .on('end', async () => {
+        await Promise.all(files.map(async ({ path, stats }) => {
+          const data = await readFile(path);
+          const checksum = hash(data);
 
+          const file = {
+            checksum,
+            path: relative(projectPath, path),
+            mode: stats.mode,
+            size: stats.size,
+          };
+
+          if(mapHashesToFiles.has(checksum)) {
+            const { files } = mapHashesToFiles.get(checksum);
+            mapHashesToFiles.set(checksum, {
+              data,
+              files: [...files, file],
+            });
+          } else {
+            mapHashesToFiles.set(checksum, {
+              data,
+              files: [file],
+            });
+          }
+        }));
+        resolve();
+      });
+  });
+  
   // flatten files
   const files = Array
     .from(mapHashesToFiles)
