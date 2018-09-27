81acdceb699a0939188db09d0d752db30aedb1ca
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2018-09-27 23:38:00 +0330

    update static projects' default port to 5000

--- src/util/get-port.js
+++ src/util/get-port.js
@@ -3,7 +3,7 @@ export default function getPort(deploymentType) {
     // @TODO should be customizable
     return ({
         node: 3000,
-        static: 80,
+        static: 5000,
         docker: 8000,
         laravel: 80
     })[deploymentType];
