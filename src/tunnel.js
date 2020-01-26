2622ab988fe4e8eb57439ce1a5cbe9a8f7c9bac0
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-01-26 21:35:05 +0330

    Add tunnel:open command

---
+++ src/tunnel.js
@@ -0,0 +1,45 @@
+const tunnel = require('tunnel-ssh');
+const npid = require('npid');
+const os = require('os');
+const fs = require('fs');
+
+const [_, __, dstHost, dstPort, localPort] = process.argv;
+
+const baseDir = `${os.homedir()}/.liara-tunnels`;
+
+try {
+  fs.mkdirSync(baseDir, { recursive: true });
+  const pid = npid.create(`${baseDir}/${localPort}.pid`);
+  pid.removeOnExit();
+  process.on('SIGINT', function () {
+    pid.remove();
+    process.exit();
+  });
+} catch (error) {
+  console.error('Creating PID file failed.');
+  console.error(error);
+}
+
+const config = {
+  username: 'liara-cli',
+  password: '41bedf70-2cb9-4642-ba75-5ee29a799d48',
+  host: dstHost,
+  port: 2220,
+  dstHost: dstHost,
+  dstPort: dstPort,
+  localHost: '127.0.0.1',
+  localPort: localPort,
+};
+
+tunnel(config, function (error) {
+  if(error) {
+    return console.error('ERROR:', error);
+  }
+  console.log('Tunnel has been established.');
+})
+.on('connection', function(info) {
+  console.log('New Connection:', info);
+})
+.on('error', function(error) {
+  console.error('FAILED:', error)
+});
