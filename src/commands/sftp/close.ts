29ceb877f3f625e62a5dfcb0b8a47915070b2214
Reza_XD <Morekhqom78@gmail.com>
2020-03-04 20:35:45 +0330

    Add SFTP command (#17)
    * added sftp command
    
    * sftp open/close completely added!
    
    * port changed, host address added!

---
+++ src/commands/sftp/close.ts
@@ -0,0 +1,34 @@
+import os from 'os'
+import fs from 'fs'
+import path from 'path'
+import chalk from 'chalk'
+
+import Command from '../../base'
+
+const baseDir = `${os.homedir()}/.liara-sftp`;
+
+export default class TunnelClose extends Command {
+  static description = 'close all open ssh tunnels'
+
+  static flags = Command.flags
+
+  async run() {
+    fs.readdirSync(baseDir).forEach(file => {
+      const pidPath = baseDir + '/' + file;
+      const pid = Number(fs.readFileSync(pidPath).toString().replace('\n', ''));
+      this.log(`Killing ${chalk.bold(`127.0.0.1:`)}${chalk.green(path.basename(file, '.pid'))} ...`)
+
+      try {
+        process.kill(pid);
+      } catch (error) {
+        // Ignore
+      }
+
+      try {
+        fs.unlinkSync(pidPath);
+      } catch (error) {
+        // Ignore
+      }
+    });
+  }
+}
