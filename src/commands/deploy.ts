8ea89a73fba3d4d3149ce913496dd70084c7b6a5
Mohammadreza <mrvashian@gmail.com>
2022-02-06 11:14:59 +0330

    refactor: collect get info

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -228,7 +228,7 @@ To file a ticket, please head to: https://console.liara.ir/tickets`)
       body.build.args = config['build-arg']
     }
 
-    body.gitInfo = collectGitInfo(config.path, this.debug)
+    body.gitInfo = await collectGitInfo(config.path, this.debug)
 
     // @ts-ignore
     body.platformConfig = mergePlatformConfigWithDefaults(config.path, config.platform, config[config.platform] || {}, this.debug)
