586c1835243198e757735981e49f59c191e9a081
Mohammadreza <mrvashian@gmail.com>
2022-06-19 19:50:19 +0430

    Good Bye Axios :)

--- src/interceptors.ts
+++ src/interceptors.ts
@@ -1,30 +1,20 @@
-import axios from 'axios'
-import {Errors} from '@oclif/core'
-import { Hooks } from 'got'
-
-axios.interceptors.response.use(response => response, error => {
-  if (error.response && error.response.status === 401) {
-    // tslint:disable-next-line: no-console
-    console.error(new Errors.CLIError(`Authentication failed.
-Please login via 'liara login' command.`).render())
-    process.exit(2)
-  }
-
-  return Promise.reject(error)
-})
+import { Errors } from "@oclif/core";
+import { Hooks } from "got";
 
 const hooks: Hooks = {
-  beforeError:[
-    error => {
-      const {response} = error;
+  beforeError: [
+    (error) => {
+      const { response } = error;
       if (response && response.statusCode === 401) {
-        console.error(new Errors.CLIError(`Authentication failed.
-Please login via 'liara login' command.`).render())
-        process.exit(2)
+        console.error(
+          new Errors.CLIError(`Authentication failed.
+Please login via 'liara login' command.`).render()
+        );
+        process.exit(2);
       }
-      return error
-    }
-  ]
-}
+      return error;
+    },
+  ],
+};
 
-export default hooks
+export default hooks;
