3c6608570c7acbee9d31cc251f3ab76adc4f8e7a
MohammadReza <mrvashian@gmail.com>
2022-06-28 12:23:40 +0430

    Add the new db:list command (#60)

---
+++ src/commands/db/list.ts
@@ -0,0 +1,57 @@
+import chalk from 'chalk';
+import { CliUx } from "@oclif/core";
+import * as shamsi from "shamsi-date-converter";
+
+import Command from "../../base";
+import IGetDatabasesResponse from '../../types/get-dbs-response';
+export default class DatabaseList extends Command {
+  static description: string | undefined = "list available databases";
+
+  static flags = {
+    ...Command.flags,
+    ...CliUx.ux.table.flags(),
+  };
+
+  static aliases: string[] = ["db:ls"];
+
+  async run() {
+    const { flags } = await this.parse(DatabaseList);
+
+    await this.setGotConfig(flags);
+
+    const {databases} = await this.got('v1/databases').json<IGetDatabasesResponse>()
+
+    if (!databases.length) {
+      this.error(`Not found any database.
+Please open up https://console.liara.ir/databases and create the database, first.`);
+    }
+
+    const databasesData = databases.map((db) => {
+      const shamsiData = shamsi.gregorianToJalali(new Date(db.created_at));
+
+      const Scale = db.scale === 1 ? chalk.green('ON') : chalk.gray('OFF');
+      const Status = db.status === "RUNNING" ? 'OK': db.status
+      return {
+        Name: db.hostname,
+        Type: db.type,
+        Plan: db.planID,
+        Status,
+        Scale,
+        "Created At": `${shamsiData[0]}-${shamsiData[1]}-${shamsiData[2]}`,
+      };
+    });
+
+    CliUx.ux.table(
+      databasesData,
+      {
+        Name: {},
+        Type: {},
+        Plan: {},
+        Scale: {},
+        Status: {},
+        "Created At": {},
+      },
+      flags
+    );
+  }
+}
---
+++ src/types/get-dbs-response.ts
@@ -0,0 +1,5 @@
+import IDatabase from './database';
+
+export default interface IGetDatabasesResponse {
+  databases: IDatabase[];
+}
