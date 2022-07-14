12299a3b8d5899a6915ec1255ee1580632a04548
Mohammadreza <mrvashian@gmail.com>
2022-07-14 09:57:51 +0430

    disalbe timeout for upload request

--- src/services/upload.ts
+++ src/services/upload.ts
@@ -6,7 +6,12 @@ import FormData from 'form-data'
 function upload (project: string, httpClient: Got, sourcePath: string) {
   const body = new FormData();
   body.append('file', fs.createReadStream(sourcePath))
-  return httpClient.post(`v2/projects/${project}/sources`, { body })
+  return httpClient.post(`v2/projects/${project}/sources`, { 
+    body,
+    timeout: {
+      request: undefined
+    }
+  })
 }
 
 export default upload
