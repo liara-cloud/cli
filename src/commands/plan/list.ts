f20f888447256d200f1bff75bd595eb529b68889
Parnian.dev <parnianshahla2004@gmail.com>
2024-06-25 11:40:50 +0330

    Fixed plan ls + Added bundle plan

--- src/commands/plan/list.ts
+++ src/commands/plan/list.ts
@@ -1,6 +1,5 @@
 import { ux } from '@oclif/core';
 import Command from '../../base.js';
-import spacing from '../../utils/spacing.js';
 
 export default class PlanList extends Command {
   static description = 'list available plans';
@@ -13,47 +12,67 @@ export default class PlanList extends Command {
   static aliases = ['plan:ls'];
 
   async run() {
-    const { flags } = await this.parse(PlanList);
+    try {
+      const { flags } = await this.parse(PlanList);
 
-    await this.setGotConfig(flags);
+      await this.setGotConfig(flags);
 
-    // TODO: Use proper type for plans
-    const { plans } = await this.got('v1/me').json<{ plans: any }>();
-    const plansData = Object.keys(plans.projects)
-      .filter(
-        (plan) =>
-          plans.projects[plan].available &&
-          plans.projects[plan].region === 'iran'
-      )
-      .map((plan) => {
-        const Plan = plan;
-        const availablePlan = plans.projects[plan];
-        const tRAM = availablePlan.RAM.amount;
-        const RAM = tRAM + spacing(5, tRAM) + 'GB';
-        const tCPU = availablePlan.CPU.amount;
-        const CPU = tCPU + spacing(5, tCPU) + 'Core';
-        const StorageClass = availablePlan.storageClass;
-        const tDisk = availablePlan.volume;
-        const Disk = tDisk
-          ? tDisk + spacing(3, tDisk) + `GB ${StorageClass}`
-          : 0;
-        const tPrice = availablePlan.price * 720;
-        const Price = tPrice
-          ? tPrice.toLocaleString() + spacing(7, tPrice) + 'Tomans/Month'
-          : 0;
-        return {
-          Plan,
-          RAM,
-          CPU,
-          Disk,
-          Price,
-        };
-      });
+      // TODO: Use proper type for plans
+      const { plans } = await this.got('v1/me').json<{ plans: any }>();
+      const plansData = Object.keys(plans.projects)
+        .filter(
+          (plan) =>
+            (plan === 'free' || plan.includes('g2')) &&
+            plans.projects[plan].available &&
+            plans.projects[plan].region === 'iran',
+        )
+        .map((plan) => {
+          const Plan = plan;
+          const BundlePlan = plans.projectBundlePlans[plan];
+          const BundlePlanDetails = Object.keys(BundlePlan).map((key) => {
+            const { displayPrice } = BundlePlan[key];
+            return { key, displayPrice };
+          });
 
-    ux.table(
-      plansData,
-      { Plan: {}, RAM: {}, CPU: {}, Disk: {}, Price: {} },
-      flags
-    );
+          const Free = BundlePlanDetails[0].displayPrice.toLocaleString();
+          const Standard = BundlePlanDetails[1].displayPrice.toLocaleString();
+          const Pro = BundlePlanDetails[2].displayPrice.toLocaleString();
+
+          const availablePlan = plans.projects[plan];
+          const RAM = availablePlan.RAM.amount;
+          const CPU = availablePlan.CPU.amount;
+          const Disk = availablePlan.volume;
+          const tPrice = availablePlan.price * 720;
+          const Price = tPrice ? tPrice.toLocaleString() : 0;
+          return {
+            Plan,
+            RAM,
+            CPU,
+            Disk,
+            Price,
+            Free,
+            Standard,
+            Pro,
+          };
+        });
+
+      ux.table(
+        plansData,
+        {
+          Plan: {},
+          RAM: { header: 'RAM(GB)' },
+          CPU: { header: 'CPU(Core)' },
+          Disk: { header: 'Disk(GB SSD)' },
+          Price: { header: 'Price(Tomans/Month)' },
+          Free: { header: 'Bronze' },
+          Standard: { header: 'Silver(Tomans/Month)' },
+          Pro: { header: 'Gold(Tomans/Month)' },
+        },
+        flags,
+      );
+    } catch (err) {
+      this.debug(err);
+      this.error('There was a problem while getting plan info.');
+    }
   }
 }
