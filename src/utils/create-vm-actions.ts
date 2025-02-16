1b07e140644eaa92a11712f5231eea22bb9afaa2
Amin Ehterami <A.Ehterami@proton.me>
2025-02-16 17:06:50 +0330

    feat: Add vm stop command

--- src/utils/create-vm-actions.ts
+++ src/utils/create-vm-actions.ts
@@ -6,17 +6,13 @@ export async function createAction(
   action: 'start' | 'shutdown' | 'reboot' | 'stop',
   gotClient: Got,
 ) {
-  const spinner = ora();
-  spinner.start('Loading...');
   try {
     await gotClient.patch(`vm/power/${vmId}`, {
       json: {
         action,
       },
     });
-    spinner.succeed('VM started successfully!');
   } catch (error) {
-    spinner.stop();
     throw new Error('Action failed.');
   }
 }
