674a229bd6a46ffdb3dbfdb2a0c863edf85ae3d2
Mortza81 <mortezaahmadinya.81@gmail.com>
2025-02-16 17:40:02 +0330

    feat: vm start command

---
+++ src/commands/vm/start.ts
@@ -0,0 +1,65 @@
+import { Args, Flags } from '@oclif/core';
+import Command, { IConfig, IGetVMResponse } from '../../base.js';
+import { IAAS_API_URL } from '../../constants.js';
+import ora from 'ora';
+import { createAction } from '../../utils/create-vm-actions.js';
+import { promptVMs } from '../../utils/prompt-vms.js';
+
+export default class VmStart extends Command {
+  static override args = {
+    file: Args.string({ description: 'file to read' }),
+  };
+  static flags = {
+    ...Command.flags,
+    vm: Flags.string({
+      char: 'v',
+      description: 'VM name',
+    }),
+    detach: Flags.boolean({
+      char: 'd',
+      description: 'run command in detach mode',
+    }),
+  };
+  static override description = 'power-on the VM.';
+
+  async setGotConfig(config: IConfig): Promise<void> {
+    await super.setGotConfig(config);
+    this.got = this.got.extend({
+      prefixUrl: IAAS_API_URL,
+    });
+  }
+
+  public async run(): Promise<void> {
+    const { args, flags } = await this.parse(VmStart);
+
+    this.spinner = ora();
+
+    await this.setGotConfig(flags);
+
+    const vms = await this.getVms(
+      'No VMs found in the shutdown state.',
+      (vm) => vm.power === 'POWERED_OFF' && vm.state !== 'DELETING',
+    );
+    const vm = await promptVMs(vms);
+    await createAction(vm._id, 'start', this.got);
+    if (flags.detach) {
+      this.spinner.succeed(`Start signal has been sent for VM "${vm.name}"`);
+      return;
+    }
+    const intervalID = setInterval(async () => {
+      this.spinner.start(`VM "${vm.name}" is starting...`);
+
+      const stopedVm = await this.got
+        .get(`vm/${vm._id}`)
+        .json<IGetVMResponse>();
+
+      if (stopedVm.power === 'POWERED_ON') {
+        this.spinner.stop();
+
+        this.spinner.succeed(`VM "${vm.name}" has been started'`);
+
+        clearInterval(intervalID);
+      }
+    }, 2000);
+  }
+}
