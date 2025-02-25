6b3333add38074b840c48ee7a398aaa698715d76
Mortza81 <mortezaahmadinya.81@gmail.com>
2025-02-25 15:36:58 +0330

    chore: add tests for app create command

--- test/units/app/create.unit.test.ts
+++ test/units/app/create.unit.test.ts
@@ -1,91 +1,173 @@
-import { runCommand } from '@oclif/test';
-import nock from 'nock';
-import { expect } from 'chai';
+import { runCommand } from "@oclif/test";
+import nock from "nock";
+import { expect } from "chai";
 import { networks } from "../../fixtures/networks/fixture.ts"
-describe('app:create', function () {
-  let api: ReturnType<typeof nock>;
-  const appName = 'test-app';
-  const platform = 'laravel';
-  const plan = 'small-g2';
-  const bundlePlan = 'standard';
-  const network = networks.networks[0].name;
-  api = nock('https://api.iran.liara.ir');
 
-  api
-    .get('/v1/networks')
-    .query({ teamID: '' })
-    .reply(200, networks);
+describe("app:create", function () {
+  const api = nock("https://api.iran.liara.ir");
 
-  api
-    .post('/v1/projects/', {
-      name: appName,
-      planID: plan,
-      platform: platform,
-      bundlePlanID: bundlePlan,
-      network: networks.networks[0]._id,
-      readOnlyRootFilesystem: true,
-    })
-    .query({ teamID: '' })
-    .reply(409,{
-      statusCode: 409,
-      error: "Conflict",
-      message: "Project exists.",
-      data: null
+
+
+  beforeEach(() => {
+    api
+      .get("/v1/networks")
+      .query({ teamID: "" })
+      .reply(200, networks);
   });
 
-  afterEach(function () {
+  afterEach(() => {
     api.done();
     nock.cleanAll();
   });
 
-    it('creates an app with the specified flags', async ()=> {
+  it.skip("creates an app with the specified flags", async () => {
 
-      const { stdout, stderr } = await runCommand([
-        'app:create',
-        '--app',
-        appName,
-        '--platform',
-        platform,
-        '--plan',
-        plan,
-        '--feature-plan',
-        bundlePlan,
-        '--network',
-        network,
-        '--read-only',
-        'true',
-      ]);
-      expect(stdout).to.equal(`App ${appName} created.\n`);
-    });
-    it.skip('throws an error if app name already exists',async ()=>{
-      api
-    .post('/v1/projects/', {
-      name: appName,
-      planID: plan,
-      platform: platform,
-      bundlePlanID: bundlePlan,
-      network: networks.networks[0]._id,
-      readOnlyRootFilesystem: true,
-    })
-    .query({ teamID: '' })
-    .reply(409);
+    api
+      .post("/v1/projects/", {
+        name: "test-app",
+        planID: "small-g2",
+        platform: "laravel",
+        bundlePlanID: "standard",
+        network: networks.networks[0]._id,
+        readOnlyRootFilesystem: true,
+      })
+      .query({ teamID: "" })
+      .reply(200);
 
-    const { stdout, stderr } = await runCommand([
-      'app:create',
-      '--app',
-      appName,
-      '--platform',
-      platform,
-      '--plan',
-      plan,
-      '--feature-plan',
-      bundlePlan,
-      '--network',
-      network,
-      '--read-only',
-      'true',
+    const { stdout } = await runCommand([
+      "app:create",
+      "--app", "test-app",
+      "--platform", "laravel",
+      "--plan", "small-g2",
+      "--feature-plan", "standard",
+      "--network", networks.networks[0].name,
+      "--read-only", "true",
     ]);
-    console.log(stderr);
-  expect(stderr).to.equal(`The app already exists. Please use a unique name for your app.`);
-    })
+    
+    expect(stdout).to.equal(`App test-app created.\n`);
   });
+
+  it.skip("throws an error if app name already exists", async () => {
+
+    api
+      .post("/v1/projects/", {
+        name: "test-app",
+        planID: "small-g2",
+        platform: "laravel",
+        bundlePlanID: "standard",
+        network: networks.networks[0]._id,
+        readOnlyRootFilesystem: true,
+      })
+      .query({ teamID: "" })
+      .reply(409, {
+        statusCode: 409,
+        error: "Conflict",
+        message: "Project exists.",
+        data: null
+      });
+
+    const { error } = await runCommand([
+      "app:create",
+      "--app", "test-app",
+      "--platform", "laravel",
+      "--plan", "small-g2",
+      "--feature-plan", "standard",
+      "--network", networks.networks[0].name,
+      "--read-only", "true",
+    ]);
+
+    expect(error?.message).to.equal("The app already exists. Please use a unique name for your app.");
+  });
+
+  it.skip("throws an error if the network is not found", async () => {
+
+    const { error } = await runCommand([
+      "app:create",
+      "--app", "test-app",
+      "--platform", "laravel",
+      "--plan", "small-g2",
+      "--feature-plan", "standard",
+      "--network", "not-found",
+      "--read-only", "true",
+    ]);
+
+    expect(error?.message).to.equal("Network not-found not found.");
+  })
+
+  it.skip("thorws an error if user select a feature plan that is not available for free plan", async () => {
+
+    const { error } = await runCommand([
+      "app:create",
+      "--app", "test-app",
+      "--platform", "laravel",
+      "--plan", "free",
+      "--feature-plan", "standard",
+      "--network", networks.networks[0].name,
+      "--read-only", "true",
+    ]);
+
+    expect(error?.message).to.equal(`Only "free" feature bundle plan is available for free plan.`);
+  }
+  )
+
+  it.skip("throws an error if the user does not have enough balance", async () => {
+
+    api
+      .post("/v1/projects/", {
+        name: "test-app",
+        planID: "small-g2",
+        platform: "laravel",
+        bundlePlanID: "standard",
+        network: networks.networks[0]._id,
+        readOnlyRootFilesystem: true,
+      })
+      .query({ teamID: "" })
+      .reply(402);
+
+    const { error } = await runCommand([
+      "app:create",
+      "--app", "test-app",
+      "--platform", "laravel",
+      "--plan", "small-g2",
+      "--feature-plan", "standard",
+      "--network", networks.networks[0].name,
+      "--read-only", "true",
+    ]);
+
+    expect(error?.message).to.equal("Not enough balance. Please charge your account.");
+  })
+
+  it("throws proper error if the app creation fails with status code 500", async () => {
+
+    api
+      .post("/v1/projects/", {
+        name: "test-app",
+        planID: "small-g2",
+        platform: "laravel",
+        bundlePlanID: "standard",
+        network: networks.networks[0]._id,
+        readOnlyRootFilesystem: true,
+      })
+      .query({ teamID: "" })
+      .reply(500);
+
+    const { error } = await runCommand([
+      "app:create",
+      "--app", "test-app",
+      "--platform", "laravel",
+      "--plan", "small-g2",
+      "--feature-plan", "standard",
+      "--network", networks.networks[0].name,
+      "--read-only", "true",
+    ]);
+
+    expect(error?.message).to.equal(`Error: Unable to Create App
+        Please try the following steps:
+        1. Check your internet connection.
+        2. Ensure you have enough balance.
+        3. Try again later.
+        4. If you still have problems, please contact support by submitting a ticket at https://console.liara.ir/tickets`)
+  })
+
+
+});
