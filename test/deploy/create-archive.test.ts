5008e881066dee157f25d6a91e2f8418c7532772
Mortza81 <mortezaahmadinya.81@gmail.com>
2025-03-25 14:47:18 +0330

    chore: complete createArchive tests

--- test/deploy/create-archive.test.ts
+++ test/deploy/create-archive.test.ts
@@ -21,9 +21,12 @@ describe('create-archive', async () => {
   it('should create an archive and ignore files and directories listed in .gitignore', async () => {
     const sourcePath = prepareTmpDirectory();
 
+    fs.writeFileSync('test/fixtures/simple-gitignore/ignore_1.html', 'test');
+    fs.writeFileSync('test/fixtures/simple-gitignore/ignore_2.html', 'test');
+
     const originFiles = getAllFiles('test/fixtures/simple-gitignore');
 
-    await createArchive(sourcePath, 'test/fixtures/simple-gitignore', 'node');
+    await createArchive(sourcePath, 'test/fixtures/simple-gitignore', 'php');
 
     fs.mkdirSync(`${sourcePath}-dir`);
 
@@ -32,17 +35,21 @@ describe('create-archive', async () => {
       cwd: `${sourcePath}-dir`,
     });
 
-    expect(originFiles).to.deep.equal(
-      originFiles.filter((val) => val != 'node_modules'),
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+
+    expect(extractedFiles).to.deep.equal(
+      originFiles.filter(
+        (val) => val != 'ignore_1.html' && val != 'ignore_2.html',
+      ),
     );
   });
 
   it('should create an archive and ignore files and directories listed in .liaraignore', async () => {
     const sourcePath = prepareTmpDirectory();
 
-    const originFiles = getAllFiles('test/fixtures/simple-gitignore');
+    const originFiles = getAllFiles('test/fixtures/liaraignore');
 
-    await createArchive(sourcePath, 'test/fixtures/simple-gitignore', 'node');
+    await createArchive(sourcePath, 'test/fixtures/liaraignore', 'php');
 
     fs.mkdirSync(`${sourcePath}-dir`);
 
@@ -51,8 +58,38 @@ describe('create-archive', async () => {
       cwd: `${sourcePath}-dir`,
     });
 
-    expect(originFiles).to.deep.equal(
-      originFiles.filter((val) => val != 'node_modules'),
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+
+    expect(extractedFiles).to.deep.equal(
+      originFiles.filter(
+        (val) => val != 'ignore_1.html' && val != 'ignore_2.html',
+      ),
+    );
+  });
+
+  it('should create an archive and ignore files and directories listed in .dockerignore', async () => {
+    const sourcePath = prepareTmpDirectory();
+
+    const originFiles = getAllFiles('test/fixtures/dockerignore');
+
+    await createArchive(sourcePath, 'test/fixtures/dockerignore', 'php');
+
+    fs.mkdirSync(`${sourcePath}-dir`);
+
+    await extract({
+      file: sourcePath,
+      cwd: `${sourcePath}-dir`,
+    });
+
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+
+    expect(extractedFiles).to.deep.equal(
+      originFiles.filter(
+        (val) =>
+          val != 'ignore_1.html' &&
+          val != 'ignore_2.html' &&
+          val != '.dockerignore',
+      ),
     );
   });
 
@@ -70,5 +107,142 @@ describe('create-archive', async () => {
 
     const extractedFiles = getAllFiles(`${sourcePath}-dir`);
     expect(extractedFiles).to.deep.equal(['file.txt']);
+  });
+
+  it('should ignore files listed in .liaraignore and disregard .gitignore and .dockerignore if present', async () => {
+    const sourcePath = prepareTmpDirectory();
+
+    fs.writeFileSync('test/fixtures/simple-gitignore/ignore_2.html', 'test');
+
+    const originFiles = getAllFiles('test/fixtures/multiple-ignores');
+
+    await createArchive(sourcePath, 'test/fixtures/multiple-ignores', 'php');
+
+    fs.mkdirSync(`${sourcePath}-dir`);
+
+    await extract({
+      file: sourcePath,
+      cwd: `${sourcePath}-dir`,
+    });
+
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+
+    expect(extractedFiles).to.deep.equal(
+      originFiles.filter(
+        (val) => val != 'ignore_1.html' && val != '.dockerignore',
+      ),
+    );
+  });
+
+  it('should respect a nested .gitignore file and apply its rules only to its directory and subdirectories', async () => {
+    const sourcePath = prepareTmpDirectory();
+
+    const originFiles = getAllFiles('test/fixtures/nested-ignore-files');
+
+    await createArchive(sourcePath, 'test/fixtures/nested-ignore-files', 'php');
+
+    fs.mkdirSync(`${sourcePath}-dir`);
+
+    await extract({
+      file: sourcePath,
+      cwd: `${sourcePath}-dir`,
+    });
+
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+
+    expect(extractedFiles).to.deep.equal(
+      originFiles.filter(
+        (val) => val != 'ignore.me' && val != 'sub1/hello.html',
+      ),
+    );
+  });
+
+  it('should ignore some files and directories in django platform', async () => {
+    const sourcePath = prepareTmpDirectory();
+
+    await createArchive(sourcePath, 'test/fixtures/python-ignores', 'django');
+
+    fs.mkdirSync(`${sourcePath}-dir`);
+
+    await extract({
+      file: sourcePath,
+      cwd: `${sourcePath}-dir`,
+    });
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+
+    expect(extractedFiles.sort()).to.deep.equal(
+      ['test.txt', '.webassets-cache', 'instance/test.txt'].sort(),
+    );
+  });
+
+  it('should ignore some files and directories in dotnet platform', async () => {
+    const sourcePath = prepareTmpDirectory();
+
+    await createArchive(sourcePath, 'test/fixtures/dotnet-ignores', 'dotnet');
+
+    fs.mkdirSync(`${sourcePath}-dir`);
+
+    await extract({
+      file: sourcePath,
+      cwd: `${sourcePath}-dir`,
+    });
+
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+    expect(extractedFiles).to.deep.equal(['test.txt']);
+  });
+
+  it('should ignore /vendor directory in php platform', async () => {
+    const sourcePath = prepareTmpDirectory();
+
+    await createArchive(sourcePath, 'test/fixtures/php-ignores', 'php');
+
+    fs.mkdirSync(`${sourcePath}-dir`);
+
+    await extract({
+      file: sourcePath,
+      cwd: `${sourcePath}-dir`,
+    });
+
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+    expect(extractedFiles).to.deep.equal(['index.php']);
+  });
+
+  it('should ignore /vendor directory in laravel platform', async () => {
+    const sourcePath = prepareTmpDirectory();
+
+    await createArchive(sourcePath, 'test/fixtures/php-ignores', 'laravel');
+
+    fs.mkdirSync(`${sourcePath}-dir`);
+
+    await extract({
+      file: sourcePath,
+      cwd: `${sourcePath}-dir`,
+    });
+
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+    expect(extractedFiles).to.deep.equal(['index.php']);
+  });
+
+  it('should ignore some files and directories in flask platform', async () => {
+    const sourcePath = prepareTmpDirectory();
+
+    await createArchive(sourcePath, 'test/fixtures/python-ignores', 'flask');
+
+    fs.mkdirSync(`${sourcePath}-dir`);
+
+    await extract({
+      file: sourcePath,
+      cwd: `${sourcePath}-dir`,
+    });
+    const extractedFiles = getAllFiles(`${sourcePath}-dir`);
+
+    expect(extractedFiles.sort()).to.deep.equal(
+      [
+        'test.txt',
+        'test.log',
+        'local_settings.py',
+        'staticfiles/test.txt',
+      ].sort(),
+    );
   });
 });
