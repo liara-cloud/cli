27cf5d2a7d37bc97fd706e5f2f77d7e513f17f59
Amin Ehterami <A.Ehterami@proton.me>
2025-08-04 12:26:30 +0330

    Feat/twofa auth (#227)

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -18,6 +18,7 @@ export default class Login extends Command {
     interactive: Flags.boolean({
       char: 'i',
       description: 'login with username/password',
+      default: false,
     }),
     browser: Flags.string({
       description: 'browser to open',
@@ -39,12 +40,7 @@ export default class Login extends Command {
       '--from-login',
     ];
 
-    if (flags.region) {
-      sendFlag.push('--region', flags.region);
-      this.log(`You're logging into "${flags.region}" region:`);
-    }
-
-    if (!flags.interactive) {
+    if (flags.interactive === false && !flags['api-token']) {
       try {
         const accounts = await this.browser(flags.browser);
 
@@ -54,7 +50,7 @@ export default class Login extends Command {
         let currentAccount;
 
         for (const account of accounts) {
-          const name = `${account.email.split('@')[0]}_${account.region}`;
+          const name = `${account.email.split('@')[0]}`;
 
           if (account.current) {
             currentAccount = name;
@@ -62,7 +58,6 @@ export default class Login extends Command {
 
           currentAccounts[name] = {
             email: account.email,
-            region: account.region,
             avatar: account.avatar,
             api_token: account.token,
             fullname: account.fullname,
@@ -75,7 +70,7 @@ export default class Login extends Command {
           JSON.stringify({
             accounts: currentAccounts,
             version: GLOBAL_CONF_VERSION,
-          })
+          }),
         );
 
         this.spinner.succeed('You have logged in successfully.');
@@ -93,7 +88,7 @@ export default class Login extends Command {
         debug(`${error.message}\n`);
 
         this.spinner.fail(
-          'Cannot open browser. Browser unavailable or lacks permissions.'
+          'Cannot open browser. Browser unavailable or lacks permissions.',
         );
       }
     }
